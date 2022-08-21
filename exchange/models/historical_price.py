6429a16389af408766c31d113357732e22145507
Fernando J. Spitz <fjspitz@gmail.com>
2022-08-20 22:33:25 -0300

    Updated currency price models

--- exchange/models/historical_price.py
+++ exchange/models/historical_price.py
@@ -1,12 +1,13 @@
 from django.db import models
-from master.models.abstract_model import AbstractModel, decimal_field
+from master.models.abstract_model import AbstractModel
 from master.models.currency import Currency
+
 
 class HistoricalPrice(AbstractModel):
     source = models.CharField(max_length=64)
     currency = models.ForeignKey(Currency, on_delete=models.DO_NOTHING, related_name='currency')
     reference_currency = models.ForeignKey(Currency, on_delete=models.DO_NOTHING, related_name='reference_currency')
-    price = decimal_field
+    price = models.DecimalField(max_digits=48, decimal_places=16)
     last_datetime = models.DateTimeField(auto_now=True) 
 
     @property
