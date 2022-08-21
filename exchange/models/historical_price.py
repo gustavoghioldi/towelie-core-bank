205c5394f73d9a536638e331445fcc2fd28db9cd
Fernando J. Spitz <fjspitz@gmail.com>
2022-08-20 22:24:48 -0300

    Typo

--- exchange/models/historical_price.py
+++ exchange/models/historical_price.py
@@ -4,7 +4,7 @@ from master.models.currency import Currency
 
 class HistoricalPrice(AbstractModel):
     source = models.CharField(max_length=64)
-    curreny = models.ForeignKey(Currency, on_delete=models.DO_NOTHING, related_name='currency')
+    currency = models.ForeignKey(Currency, on_delete=models.DO_NOTHING, related_name='currency')
     reference_currency = models.ForeignKey(Currency, on_delete=models.DO_NOTHING, related_name='reference_currency')
     price = decimal_field
 
